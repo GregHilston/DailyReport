a068da4836eddaece0c4ee1c36e06a1ed42ca942
GregHilston <Grehgh@gmail.com>
2015-03-11 16:36:58 -0400

    Added Internet permission. Successfully using /kushanj1/ForecastIOLibrary to create the correct ForecastIO URL and getting the JSON response. Now need to parse it correctly for what we are looking for.

--- app/src/main/java/com/greghilston/dailyreport/MainActivity.java
+++ app/src/main/java/com/greghilston/dailyreport/MainActivity.java
@@ -9,8 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import com.greghilston.dailyreport.Forecast.io.v2.network.services.ForecastService;
-import com.greghilston.dailyreport.Forecast.io.v2.transfer.LatLng;
+
+import com.greghilston.dailyreport.ForecastIOLibrary.src.com.arcusweather.forecastio.ForecastIO;
+import com.greghilston.dailyreport.ForecastIOLibrary.src.com.arcusweather.forecastio.ForecastIODataPoint;
+import com.greghilston.dailyreport.ForecastIOLibrary.src.com.arcusweather.forecastio.ForecastIOResponse;
+
+import java.util.HashMap;
 
 public class MainActivity extends Activity {
     Project project = new Project("Construction", "ACME");
@@ -46,21 +50,46 @@ public class MainActivity extends Activity {
                 System.out.println("Weather Observation Button Pressed!");
 
                 String API_KEY = "cbbd1fc614026e05d5429175cdfb0d10";
+                Double Lat = 43.1339;
+                Double Lang = 70.9264;
 
-                ForecastService.Builder forcastServiceBuilder = new ForecastService.Builder(API_KEY);
-                LatLng.Builder latLngBuilder = new LatLng.Builder();
+                //Set the API key, Lat, and Lang
+                ForecastIO forecast = new ForecastIO(API_KEY,Lat, Lang);
 
-                // Durham, NH
-                latLngBuilder.setLatitude(43.1339);
-                latLngBuilder.setLongitude(70.9264);
+                //Set the request parameters
+                //ability to set the units, exclude blocks, extend options and user agent for the request. This is not required.
+                HashMap<String, String> requestParam = new HashMap<String, String>();
+                requestParam.put("units", "us");
+                requestParam.put("userAgent", "Custom User Agent 1.0");
+                forecast.setRequestParams(requestParam);
+                forecast.makeRequest();
 
-                //Build the Lat & Lang
-                LatLng ll = new LatLng(latLngBuilder);
+                String responseString = forecast.getResponseString();
+                ForecastIOResponse FIOR = new ForecastIOResponse(responseString);
 
-                //Set the Lat and Lang
-                forcastServiceBuilder.setLatLng(ll);
-                ForecastService.Request r = forcastServiceBuilder.build();
+                /*
+                //The library provides an easy way to access values as strings and data points as a list.
+                String currentSummary = FIOR.getValue("current-summary");
 
+                String thirdHourlyTemperature = FIOR.getValue("hourly-2-temperature");
+                String firstDailyIcon = FIOR.getValue("daily-0-icon");
+
+                //alerts defaults to first alert if not given an index. (Usually there is only one alert).
+                String alertDescription = FIOR.getValue("alerts-description");
+
+                ForecastIODataPoint[] minutelyPoints = FIOR.getDataPoints("minutely");
+                double thirtiethMinutePrecipitation = minutelyPoints[29].getValueAsDouble("precipitationIntensity");
+
+                ForecastIODataPoint[] hourlyPoints = FIOR.getDataPoints("hourly");
+                ForecastIODataPoint[] dailyPoints = FIOR.getDataPoints("daily");
+
+
+                //you can also do it the hard way
+                //String currentSummary = FIOR.getCurrently().getValue("summary");
+                //String firstDailyIcon = FIOR.getDaily().getData[0].getValue("icon");
+                */
+
+                System.out.print("Response String: " + responseString);
                 // TODO: Finish completing the request
             }
         });
