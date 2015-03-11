89f2646b9be7c309b07de0ae505cf99b9c773470
EvanGraham <evangraham16@gmail.com>
2015-03-11 16:18:56 -0400

    Changed the waether library to something more user friendly

---
+++ app/src/main/java/com/greghilston/dailyreport/ForecastIOLibrary/src/com/arcusweather/forecastio/ForecastIOResponse.java
@@ -0,0 +1,296 @@
+package com.greghilston.dailyreport.ForecastIOLibrary.src.com.arcusweather.forecastio;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ForecastIOResponse {
+	private ForecastIOCurrently mOutputCurrently;
+	private ForecastIOMinutely mOutputMinutely;
+	private ForecastIOHourly mOutputHourly;
+	private ForecastIODaily mOutputDaily;
+	private ForecastIOAlerts mOutputAlerts;
+	private ForecastIOFlags mOutputFlags;
+	private String latitude;
+	private String longitude;
+	private String timezone;
+	private String offset;
+	
+	/**
+	 * Constructor method which populates the child object
+	 * 
+	 * @param responseString json string from api call.
+	 */
+	public ForecastIOResponse(String responseString) {
+        JSONObject forecastJsonObject = null;
+        try {
+			forecastJsonObject = new JSONObject(responseString);
+		} catch (JSONException e) {
+			return;
+		}
+        
+        try {
+        	latitude = forecastJsonObject.getString("latitude");
+		} catch (JSONException e) {
+		}
+        
+        try {
+        	longitude = forecastJsonObject.getString("longitude");
+		} catch (JSONException e) {
+		}
+        
+        try {
+        	timezone = forecastJsonObject.getString("timezone");
+		} catch (JSONException e) {
+		}
+        
+        try {
+        	offset = forecastJsonObject.getString("offset");
+		} catch (JSONException e) {
+		}
+        
+        try {
+			JSONObject currentlyJSONObject = forecastJsonObject.getJSONObject("currently");
+	        mOutputCurrently = buildForecastIOCurrently(currentlyJSONObject);
+		} catch (JSONException e) {
+		}
+        
+    	try {
+			JSONObject minutelyJSONObject = forecastJsonObject.getJSONObject("minutely");
+	        mOutputMinutely = buildForecastIOMinutely(minutelyJSONObject);
+		} catch (JSONException e) {
+		} 
+        
+    	try {
+			JSONObject hourlyJSONObject = forecastJsonObject.getJSONObject("hourly");
+	        mOutputHourly = buildForecastIOHourly(hourlyJSONObject);
+		} catch (JSONException e) {
+		} 
+
+    	try {
+			JSONObject dailyJSONObject = forecastJsonObject.getJSONObject("daily");
+	        mOutputDaily = buildForecastIODaily(dailyJSONObject);
+		} catch (JSONException e) {
+		} 
+
+        try {
+        	JSONArray alertsJSONArray = forecastJsonObject.getJSONArray("alerts");
+	        mOutputAlerts = buildForecastIOAlerts(alertsJSONArray);
+        } catch (JSONException e) {
+        }
+        
+        try {
+        	JSONObject flagsJSONObject = forecastJsonObject.getJSONObject("flags");
+        	mOutputFlags = buildForecastIOFlags(flagsJSONObject);
+        } catch (JSONException e) {
+        }
+        
+	}
+
+	/**
+	 * This method will return a specific value based on the key string.
+	 * This method will return null if the value is not found.
+	 * @param keyString
+	 * @return the value of the data point
+	 */
+	public String getValue(String keyString) {
+		String[] fields = keyString.split("-");
+		String level = fields[0];
+		String value = null;
+
+		try {
+			if(level.equals(new String("latitude"))) {
+				value = latitude;
+			}
+			else if(level.equals(new String("longitude"))) {
+				value = longitude;
+			}
+			else if(level.equals(new String("timezone"))) {
+				value = timezone;
+			}
+			else if(level.equals(new String("offset"))) {
+				value = offset;
+			}
+			else if(level.equals(new String("currently"))) {
+				value = getCurrently().getValue(fields[1]);
+			}
+			else if(level.equals(new String("minutely"))) {
+				try {
+					int listIndex = Integer.parseInt(fields[1]);
+					value = getMinutely().getData()[listIndex].getValue(fields[2]);
+				}
+				catch(NumberFormatException e) {
+					value = getMinutely().getValue(fields[1]);
+				}
+			}
+			else if(level.equals(new String("hourly"))) {
+				try {
+					int listIndex = Integer.parseInt(fields[1]);
+					value = getHourly().getData()[listIndex].getValue(fields[2]);
+				}
+				catch(NumberFormatException e) {
+					value = getHourly().getValue(fields[1]);
+				}
+			}
+			else if(level.equals(new String("daily"))) {
+				try {
+					int listIndex = Integer.parseInt(fields[1]);
+					value = getDaily().getData()[listIndex].getValue(fields[2]);
+				}
+				catch(NumberFormatException e) {
+					value = getDaily().getValue(fields[1]);
+				}
+			}
+			else if(level.equals(new String("alerts"))) {
+				try {
+					int listIndex = Integer.parseInt(fields[1]);
+					value = getAlerts().getData()[listIndex].getValue(fields[2]);
+				}
+				catch(NumberFormatException e) {
+					value = getAlerts().getData()[0].getValue(fields[1]);
+				}
+			}
+			else if(level.equals(new String("flags"))) {
+				value = getFlags().getValue(fields[1]);
+			}
+		}
+		catch(NullPointerException e) {
+			return null;
+		}
+		return value;
+	}
+	
+	/**
+	 * method which returns a list of ForecastIODataPoint 
+	 * @param keyString string which defines what data points are required.
+	 * @return
+	 */
+	public ForecastIODataPoint[] getDataPoints(String keyString) {
+		ForecastIODataPoint[] value = null;
+		try {
+			if(keyString == "minutely") {
+				value = getMinutely().getData();
+			}
+			else if(keyString == "hourly") {
+				value = getHourly().getData();
+			}
+			else if(keyString == "daily") {
+				value = getDaily().getData();
+			}
+		}
+		catch(NullPointerException e) {
+			return null;
+		}
+		return value;
+	}
+	
+	/**
+	 * method which returns the currently object
+	 * @return
+	 */
+	public ForecastIOCurrently getCurrently() {
+		return mOutputCurrently;
+	}
+	
+	/**
+	 * method which sets up the currently object
+	 * @param forecastJsonObject json object used to create object
+	 * @return	ForecastIOCurrently object
+	 * @see ForecastIOCurrently
+	 */
+	public ForecastIOCurrently buildForecastIOCurrently(JSONObject forecastJsonObject) {
+		return new ForecastIOCurrently(forecastJsonObject);
+	}
+
+	/**
+	 * method which returns the minutely object
+	 * @return
+	 */
+	public ForecastIOMinutely getMinutely() {
+		return mOutputMinutely;
+	}
+	
+	/**
+	 * method which sets up and populates the response object
+	 * @param forecastJsonObject input json object for parsing
+	 * @return ForecastIOMinutely object which contains all flag info
+	 * @see ForecastIOMinutely
+	 */
+	public ForecastIOMinutely buildForecastIOMinutely(JSONObject forecastJsonObject) {
+		return new ForecastIOMinutely(forecastJsonObject);
+	}
+
+	/**
+	 * method which returns hourly object
+	 * @return
+	 */
+	public ForecastIOHourly getHourly() {
+		return mOutputHourly;
+	}
+	
+	/**
+	 * method which sets up and populates the response object
+	 * @param forecastJsonObject input json object for parsing
+	 * @return ForecastIOHourly object which contains all flag info
+	 * @see ForecastIOHourly
+	 */
+	public ForecastIOHourly buildForecastIOHourly(JSONObject forecastJsonObject) {
+		return new ForecastIOHourly(forecastJsonObject);
+	}
+
+	/**
+	 * method which returns the daily object in api call.
+	 * @return
+	 */
+	public ForecastIODaily getDaily() {
+		return mOutputDaily;
+	}
+	
+	/**
+	 * method which sets up and populates the response object
+	 * @param forecastJsonObject input json object for parsing
+	 * @return ForecastIODaily object which contains all flag info
+	 * @see ForecastIODaily
+	 */
+	public ForecastIODaily buildForecastIODaily(JSONObject forecastJsonObject) {
+		return new ForecastIODaily(forecastJsonObject);
+	}
+
+	/**
+	 * method which returns a list of ForecastIOAlerts
+	 * @return ForecastIOAlerts object which contains all alert info
+	 * @see ForecastIOAlerts
+	 */
+	public ForecastIOAlerts getAlerts() {
+		return mOutputAlerts;
+	}
+	
+	/**
+	 * method which sets up and populates the response object
+	 * @param forecastJsonArray input json object for parsing
+	 * @return ForecastIOAlerts object which contains all flag info
+	 * @see ForecastIOAlerts
+	 */
+	public ForecastIOAlerts buildForecastIOAlerts(JSONArray forecastJsonArray) {
+		return new ForecastIOAlerts(forecastJsonArray);
+	}
+	
+	/**
+	 * method which returns a list of ForecastIOFlags
+	 * @return ForecastIOFlags object which contains all flag info
+	 * @see ForecastIOFlags
+	 */
+	public ForecastIOFlags getFlags() {
+		return mOutputFlags;
+	}
+	
+	/**
+	 * method which sets up and populates the response object
+	 * @param forecastJsonObject input json object for parsing
+	 * @return ForecastIOFlags object which contains all flag info
+	 * @see ForecastIOFlags
+	 */
+	public ForecastIOFlags buildForecastIOFlags(JSONObject forecastJsonObject) {
+		return new ForecastIOFlags(forecastJsonObject);
+	}
+}
--- app/src/main/java/com/greghilston/dailyreport/WeatherTester.java
+++ app/src/main/java/com/greghilston/dailyreport/WeatherTester.java
@@ -1,30 +1,55 @@
 package com.greghilston.dailyreport;
 
-import com.greghilston.dailyreport.Forecast.io.v2.network.services.ForecastService;
-import com.greghilston.dailyreport.Forecast.io.v2.transfer.LatLng;
+
+import com.greghilston.dailyreport.ForecastIOLibrary.src.com.arcusweather.forecastio.ForecastIO;
+import com.greghilston.dailyreport.ForecastIOLibrary.src.com.arcusweather.forecastio.ForecastIODataPoint;
+import com.greghilston.dailyreport.ForecastIOLibrary.src.com.arcusweather.forecastio.ForecastIOResponse;
+
+import java.util.HashMap;
 
 /**
  * Created by ejo78 on 2/28/15.
  */
 public class WeatherTester {
     static String API_KEY = "cbbd1fc614026e05d5429175cdfb0d10";
+    static Double Lat = 43.1339;
+    static Double Lang = 70.9264;
+
     
     public static void main(String args[]) {
-        ForecastService.Builder forcastServiceBuilder = new ForecastService.Builder(API_KEY);
-        LatLng.Builder latLngBuilder = new LatLng.Builder();
-        
-        // Durham, NH
-        latLngBuilder.setLatitude(43.1339);
-        latLngBuilder.setLongitude(70.9264);
-        
-        //Build the Lat & Lang
-        LatLng ll = new LatLng(latLngBuilder);
-        
-        //Set the Lat and Lang 
-        forcastServiceBuilder.setLatLng(ll);
-        ForecastService.Request r = forcastServiceBuilder.build();
-        
-        //Building the request~ I think
-        r.getUri();
+
+        //Set the API key, Lat, and Lang
+        ForecastIO forecast = new ForecastIO(API_KEY,Lat, Lang);
+
+        //Set the request parameters
+        //ability to set the units, exclude blocks, extend options and user agent for the request. This is not required.
+        HashMap<String, String> requestParam = new HashMap<String, String>();
+        requestParam.put("units", "us");
+        requestParam.put("userAgent", "Custom User Agent 1.0");
+        forecast.setRequestParams(requestParam);
+        forecast.makeRequest();
+
+        String responseString = forecast.getResponseString();
+        ForecastIOResponse FIOR = new ForecastIOResponse(responseString);
+
+        //The library provides an easy way to access values as strings and data points as a list.
+        String currentSummary = FIOR.getValue("current-summary");
+        String thirdHourlyTemperature = FIOR.getValue("hourly-2-temperature");
+        String firstDailyIcon = FIOR.getValue("daily-0-icon");
+
+        //alerts defaults to first alert if not given an index. (Usually there is only one alert).
+        String alertDescription = FIOR.getValue("alerts-description");
+
+        ForecastIODataPoint[] minutelyPoints = FIOR.getDataPoints("minutely");
+        double thirtiethMinutePrecipitation = minutelyPoints[29].getValueAsDouble("precipitationIntensity");
+
+        ForecastIODataPoint[] hourlyPoints = FIOR.getDataPoints("hourly");
+        ForecastIODataPoint[] dailyPoints = FIOR.getDataPoints("daily");
+
+        //you can also do it the hard way
+        //String currentSummary = FIOR.getCurrently().getValue("summary");
+        //String firstDailyIcon = FIOR.getDaily().getData[0].getValue("icon");
+
+        System.out.print(currentSummary);
     }
 }
