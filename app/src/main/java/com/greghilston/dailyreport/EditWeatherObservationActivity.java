cf899fbb90352b19052d0f547ec58082a03ab8a3
EvanGraham <evangraham16@gmail.com>
2015-04-17 00:44:23 -0400

    Updated the input keyboards for editing the observations.

--- app/src/main/java/com/greghilston/dailyreport/EditWeatherObservationActivity.java
+++ app/src/main/java/com/greghilston/dailyreport/EditWeatherObservationActivity.java
@@ -1,13 +1,24 @@
 package com.greghilston.dailyreport;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class EditWeatherObservationActivity extends Activity {
+import java.util.Calendar;
+
+public class EditWeatherObservationActivity extends FragmentActivity {
     Button submitButton;
     Button cancelButton;
     Button removeButton;
@@ -25,6 +36,7 @@ public class EditWeatherObservationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_weather_observation);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
         submitButton = (Button) findViewById(R.id.button2);
         cancelButton = (Button) findViewById(R.id.button);
@@ -90,7 +102,44 @@ public class EditWeatherObservationActivity extends Activity {
                 finish();
             }
         });
+
+
     }
+
+    //TODO: This is not the "Correct" way to do this but it is working
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+            TextView textView;
+        TimePickerFragment(TextView textView){
+            this.textView=textView;
+        }
+
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            //DateFormat.is24HourFormat(getActivity())
+            return new TimePickerDialog(getActivity(), this, hour, minute,true);
+        }
+
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            textView.setText(hourOfDay + ":" + minute);
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment(timeEditText);
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
+
 }
 
 
